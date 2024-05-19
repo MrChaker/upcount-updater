cd1a0e82491a090f0b56ccf57bda378f456ce2bc
MrChaker <mr.chakerw@gmail.com>
2024-05-19 14:25:51 +0100

    Init commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const fs = require("fs");
+
+const app = express();
+
+app.get("/:target/:arch/:current_version", (req, res) => {
+  let { target, arch, current_version } = req.params;
+
+  fs.readFile("updater.json", "utf8", (err, data) => {
+    if (err) {
+      console.error("Error reading file:", err);
+      res.status(204);
+    }
+
+    const jsonData = JSON.parse(data);
+    if (current_version < jsonData.version) {
+      res.status(200).send(jsonData);
+    }
+    res.status(204);
+  });
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port http://localhost:${PORT}`);
+});
